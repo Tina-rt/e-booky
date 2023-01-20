12fda63fbfd83df374a0263703426709c509ee06
LucaZH <lucarandria1@gmail.com>
2023-01-20 17:08:37 +0300

    [Fix] Url Database

--- src/main/java/com/ebooky/dbconn/Connexion.java
+++ src/main/java/com/ebooky/dbconn/Connexion.java
@@ -5,17 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connexion {
+	
 	private static String url = "jdbc:sqlite:booky.db";
     private static Connection con;
 
     public static Connection getConnexion() {
         try {
+        	
+        	Class.forName("org.sqlite.JDBC");
             con = DriverManager.getConnection(url);
             System.out.println("Connexion établie avec succès à la base de données.");
         } catch (SQLException e) {
             System.out.println("Impossible d'établir une connexion à la base de données.");
             e.printStackTrace();
-        }
+        } catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 		return con;
     }
     
